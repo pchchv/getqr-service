b9a0d39ae5926c3e352c89e0ec45a83876e76c99
pchchv <ipchchv@gmail.com>
2022-08-23 11:36:26 +0300

    Implement a ping handler func

--- server.go
+++ server.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
+// Checks that the server is up and running
+func pingHandler(c echo.Context) error {
+	message := "QR Code creation service. Version 0.0.1"
+	return c.String(http.StatusOK, message)
+}
+
 // The declaration of all routes comes from it
-func routes(e *echo.Echo) {}
+func routes(e *echo.Echo) {
+	e.GET("/", pingHandler)
+	e.GET("/ping", pingHandler)
+}
 
 func server() {
 	e := echo.New()
