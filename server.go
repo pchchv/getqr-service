02182b4c7bfde88dde53efa130e7e1b210a8bb39
pchchv <ipchchv@gmail.com>
2022-08-23 11:57:34 +0300

    Implement getQR handler

--- server.go
+++ server.go
@@ -14,10 +14,16 @@ func pingHandler(c echo.Context) error {
 	return c.String(http.StatusOK, message)
 }
 
+func getQRHandler(c echo.Context) error {
+	qr := getQR()
+	return c.File(qr)
+}
+
 // The declaration of all routes comes from it
 func routes(e *echo.Echo) {
 	e.GET("/", pingHandler)
 	e.GET("/ping", pingHandler)
+	e.GET("/qr", getQRHandler)
 }
 
 func server() {
