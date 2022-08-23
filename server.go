49356339dac3c8b56a3ce1775f43a447f2f80c3d
pchchv <ipchchv@gmail.com>
2022-08-23 11:29:30 +0300

    Implement the routes function. The declaration of all routes comes from it.

--- server.go
+++ server.go
@@ -7,8 +7,12 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
+// The declaration of all routes comes from it
+func routes(e *echo.Echo) {}
+
 func server() {
 	e := echo.New()
+	routes(e)
 	e.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(1000)))
 	log.Fatal(e.Start(":" + getEnvValue("PORT")))
 }
